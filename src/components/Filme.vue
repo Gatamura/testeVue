56631d6bf17c8398880a437620cbfd1837ec633a
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-01-26 01:52:06 -0200

    teste

---
+++ src/components/Filme.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div class="gui-card" v-bind:style="efeitoClick" v-on:click="clickEfeito" v-on:mouseout="removeClickEfeito">
+        <div class="gui-card__media">
+            <img class="gui-card__img" v-bind:src="imagem" alt=""  />
+        </div>
+        <div class="gui-card__details">
+            <div class="gui-card__title">
+                {{titulo}}
+            </div>
+        </div>
+    </div> 
+  
+</template>
+
+<script>
+    export default {
+        name: 'teste',
+        props: ['titulo', 'imagem'],
+        data(){
+            return{
+                efeitoClick : null,
+            }
+        },
+        methods: {
+            clickEfeito: function(){
+                console.log('clickEfeito')
+                this.efeitoClick = 'transform: scale(1.9); margin-right: 100px;'
+            },
+            removeClickEfeito: function(){
+                console.log('removeClickEfeito')
+                this.efeitoClick = null;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+h1{
+    color: red
+}
+
+</style>
